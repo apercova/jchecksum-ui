997589790abb74c6e0b91ab934246fda7dcac0d5
apercova <alonsopc91@gmail.com>
2019-04-25 01:12:04 -0500

    improve init scripts

--- script/jchecksumui.cmd
+++ script/jchecksumui.cmd
@@ -2,4 +2,4 @@
 if "%JAVA_HOME%"=="" (
 set /p JAVA_HOME="JAVA_HOME: [%JAVA_HOME%]"
 )
-%JAVA_HOME%\bin\java -jar %~dp0\jchecksum-ui-1.0.1904.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+start /b %JAVA_HOME%\bin\javaw -jar %~dp0\jchecksum-ui-1.0.1904.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
